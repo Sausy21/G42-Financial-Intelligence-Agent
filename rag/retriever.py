ac9c32d2d61e536c91c83bcdeb8cccb9e7323c6a
Sausy21 <sausthava@gmail.com>
2026-05-05 12:36:15 +0400

    fix: remove self.backend assignment that conflicts with @property

--- rag/retriever.py
+++ rag/retriever.py
@@ -457,7 +457,6 @@ class HybridRetriever:
         self.dense_weight = dense_weight
         self.sparse_weight = sparse_weight
         self._query_cache: dict[str, np.ndarray] = {}
-        self.backend = "hybrid"
 
     @property
     def backend(self) -> str:
